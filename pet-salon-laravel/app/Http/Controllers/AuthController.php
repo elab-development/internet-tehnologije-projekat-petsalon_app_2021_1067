88d86d93dfb1964f928dc5366a8273d355fb8f32
Luka Mirkovic <lukaolimpik@gmail.com>
2024-01-14 21:46:39 +0100

    Uradjen kontroler za autentifikaciju i dodate rute u api.php

--- pet-salon-laravel/app/Http/Controllers/AuthController.php
+++ pet-salon-laravel/app/Http/Controllers/AuthController.php
@@ -3,8 +3,93 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
 
 class AuthController extends Controller
 {
-    //
+    //fja za registraciju
+    public function register(Request $request)
+    {
+        $validator=Validator::make($request->all(),[
+            'name'=>'required|string',
+            'email'=>'required|string|email|unique:users',
+            'password'=>'required|string'
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors());
+        }
+
+        $user=User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>Hash::make($request->password)
+        ]);
+
+        $token=$user->createToken('auth_token')->plainTextToken;
+        
+        return response()->json([
+            'Poruka' => 'Uspesno ste se registrovali',
+            'user'=>$user,
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+    //fja za logovanje na app
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['Greska:', $validator->errors()]);
+        }
+
+        if(!Auth::attempt($request->only('email','password'))){
+            return response()->json(['Poruka'=>'Nedozvoljeno'], 401);
+        }
+
+        $user = User::where('email', $request['email']) -> firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'Poruka' => 'Zdravo,' . $user->name . ' na aplikaciju Salon za pse',
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+    //fja za resetovanje lozinke ako je zaboravljena
+    public function forgotPassword(Request $request)
+    {   
+        $request->validate([
+            'email' => 'required',
+            'new_password' => 'required|string|min:8'
+        ]);
+    
+        $user = User::where('email', $request->email)->first();
+    
+        if ($user) {
+            $user->password = Hash::make($request->new_password);
+            $user->save();
+            
+            return response()->json(['Poruka' => 'Uspesno resetovana lozinka']);
+        }
+    
+        return response()->json(['Poruka' => 'Nije pronadjen korisnik'], 404);
+    }
+
+    //fja za logout
+    public function logout(Request $request)
+    {
+       $request->user()->tokens()->delete();
+       return response()->json(['Poruka'=> 'Uspesan logout']);
+    }
 }
--- pet-salon-laravel/routes/api.php
+++ pet-salon-laravel/routes/api.php
@@ -1,19 +1,19 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+//rute kojima svako moze da pristupi
+Route::post('/register',[AuthController::class,'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('forgotPassword',[AuthController::class,'forgotPassword']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+//rute za koje mora korisnik da bude autentifikovan - da ima token
+//da li je radnik ili klasican proveravamo u kontrolerima
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+     Route::post('/logout', [AuthController::class, 'logout']);  
+
 });
